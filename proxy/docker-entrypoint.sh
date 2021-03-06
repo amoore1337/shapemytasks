d941b62fb9e711b15b56a195ea42d6f3c2bd9cbf
Austin Moore <amoore1337@gmail.com>
2021-03-06 18:11:05 -0500

    setup stuff for certbot in prod

--- proxy/docker-entrypoint.sh
+++ proxy/docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${REVERSE_PROXY_SCHEME} ${SERVER_PORT} ${CLIENT_PORT}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
+if "$USE_CERTBOT" == "true"
+then
+  envsubst '${REVERSE_PROXY_SCHEME} ${SERVER_PORT} ${CLIENT_PORT}' < /etc/nginx/conf.d/nginx.conf.certbot.template > /etc/nginx/conf.d/default.conf
+else
+  envsubst '${REVERSE_PROXY_SCHEME} ${SERVER_PORT} ${CLIENT_PORT}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
+fi
+
 
 exec "$@"
